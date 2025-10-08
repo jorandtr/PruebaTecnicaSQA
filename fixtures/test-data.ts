1d51f2f7a9a7952068cb5605d7b8dcf882f1a28a
jorandtr <Jorandtr@gmail.com>
2025-10-08 09:40:29 -0500

    Cambios en los datos de prueba

---
+++ fixtures/test-data.ts
@@ -0,0 +1,10 @@
+export const testData = {
+  categorias: {
+    amor: 'Amor',
+  },
+
+  productos: {
+    producto1: 'MDF 0001',
+    producto2: 'MDF 00010',
+  }
+};
--- tests/e2e/amor.spec.ts
+++ tests/e2e/amor.spec.ts
@@ -2,18 +2,23 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../../pages/home.page';
 import { CategoriaPage } from '../../pages/categoria.page';
 import { CarritoPage } from '../../pages/carrito.page';
+import { testData } from '../../fixtures/test-data';
 test('Seleccionar dos productos de la categoría “Amor” y agregarlos al carrito', async ({ page }) => {
+  
   const home = new HomePage(page);
   const categoria = new CategoriaPage(page);
   const carrito = new CarritoPage(page);
+  const categorias = testData.categorias.amor;
+  const producto1 = testData.productos.producto1;
+  const producto2 = testData.productos.producto2;
 
   await home.irAHome();
-  await home.abrirCategoria('Amor');
-  await categoria.agregarProductoAlCarrito('MDF 0001');
-  await home.abrirCategoria('Amor');
-  await categoria.agregarProductoAlCarrito('MDF 00010');
+  await home.abrirCategoria(categorias);
+  await categoria.agregarProductoAlCarrito(producto1);
+  await home.abrirCategoria(categorias);
+  await categoria.agregarProductoAlCarrito(producto2);
   await carrito.validarCantidadDeItemsConValue();
-  await carrito.validarNombresVisiblesEnPantalla('MDF 0001');
-  await carrito.validarNombresVisiblesEnPantalla('MDF 00010');
+  await carrito.validarNombresVisiblesEnPantalla(producto1);
+  await carrito.validarNombresVisiblesEnPantalla(producto2);
   await carrito.validarSubtotal();
 });
